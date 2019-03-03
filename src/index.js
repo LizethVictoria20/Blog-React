606581193cafa42be9d3a0cd1687dd6e5e0bab03
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-03 12:24:08 -0500

    Extracción de componentes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const element = (
-  <div>
-    <h1>Hello World</h1>
-    <h2>It is {new Date().toLocaleTimeString()}.</h2>
-  </div>
-);
+/*function Welcome(props) {
+  return <h1>Mi name is {props.name}</h1>;
+}
 
+function App() {
+  return (
+    <div>
+      <Welcome name="Gordis" />
+      <Welcome name="Liz" />
+    </div>
+  );
+}*/
+
+function Avatar(props) {
+  return (
+    <img
+      className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.author}
+    />
+  );
+}
+function UserInfo(props) {
+  return (
+    <div>
+      <div className="UserInfo">
+        <Avatar user="author" />
+        <div className="UserInfo-name">{props.user}</div>
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div>
+      <UserInfo user={props.author} />
+      <div className="Comment">
+        <div className="Comment-text">{props.text}</div>
+        <div className="Comment-date">{props.date}</div>
+      </div>
+    </div>
+  );
+}
+
+const element = <Comment user="John K" date={new Date().toDateString()} />;
 ReactDOM.render(element, document.getElementById("root"));
