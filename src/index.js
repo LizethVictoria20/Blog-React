ab157ec83edd90a50cbfb9a03dbd91011170ed4d
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-03 11:41:59 -0500

    Presentacion de objetos

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function formatName(dataUser) {
-  if (dataUser) {
-    return <h1>My name is {user.firstName + user.lastName}</h1>;
-  } else {
-    return <h1>Hello Stranger</h1>;
-  }
-}
-const user = {
-  firstName: "Liz",
-  lastName: "Victoria"
-};
+const element = React.createElement(
+  "h1",
+  { className: "myClass" },
+  "Hello World"
+);
 
-const element = formatName(user);
+/*
+{
+  type: "h1",
+  props: {
+    className: "myClass",
+    children: "Liz"
+  }
+};
+*/
+
 ReactDOM.render(element, document.getElementById("root"));
