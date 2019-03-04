08c3f11077670e8c9160a0b9feae26699a052bd8
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2019-03-04 10:47:04 -0500

    Styled

--- src/index.js
+++ src/index.js
@@ -3,17 +3,37 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import styled from "styled-components";
 
+const Imagen = styled.img`
+  display: block;
+  margin: 0 auto;
+`;
 const Button = styled.button`
   background: tomato;
+  width: 100px;
+  height: 30px;
+  border-radius: 5px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  margin-bottom: 20px;
+  font-size: 15px;
 `;
 
 const DivContainer = styled.div`
   border: 1px solid tomato;
+  width: 700px;
+  margin: 0 auto;
+  border-radius: 5px;
+`;
+
+const Name = styled.h2`
+  text-align: center;
 `;
 
 function Avatar(props) {
   console.log(props);
-  return <img src={props.avatar.url} alt={props.avatar.alt} width="150px" />;
+  return <Imagen src={props.avatar.url} alt={props.avatar.alt} width="150px" />;
 }
 
 function Container(props) {
@@ -26,8 +46,8 @@ function Container(props) {
           alt: "Avatar"
         }}
       />
+      <Name>{props.name}</Name>
       <Button>Mi Boton 1</Button>
-      <h3>{props.name}</h3>
     </DivContainer>
   );
 }
